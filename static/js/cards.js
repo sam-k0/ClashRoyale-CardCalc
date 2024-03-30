5f87bf5c1244eb252db1d3ef158b1bd34928904b
sam-k0 <56673835+sam-k0@users.noreply.github.com>
2024-03-30 17:49:28 +0100

    Scheiss html gegarbel

--- static/js/cards.js
+++ static/js/cards.js
@@ -13,10 +13,16 @@ document.addEventListener('DOMContentLoaded', function() {
         // Loop through each card item
         cardItems.forEach(function(cardItem) {
             var cardRarity = cardItem.querySelector('.card-rarity').textContent.toLowerCase();
+
+            console.log(cardRarity);
             // Show or hide card based on the selected rarity
+            console.log("comparing " + rarity + " with " + cardRarity)
+
             if (rarity === 'all' || cardRarity === rarity) {
+                console.log('show');
                 cardItem.style.display = 'block'; // Show card
             } else {
+                console.log('hide');
                 cardItem.style.display = 'none'; // Hide card
             }
         });
@@ -25,7 +31,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Add event listener to filter select
     filterSelect.addEventListener('change', function() {
         filterCards(this.value);
-        print("tsts")
     });
 
     // Initial filter to display all cards
