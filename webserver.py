f8ae1369ccd31b599c39a7b6f398e39215fb130d
sam-k0 <56673835+sam-k0@users.noreply.github.com>
2024-03-30 12:02:08 +0100

    more styling

--- webserver.py
+++ webserver.py
@@ -16,11 +16,24 @@ def process():
     player:cr.Player = session.get_player(user_input)
     cards:list = player.get_cards()
     cards_parsed = [] # conains a dict with the card name, rarity, level and count
+    
     for card in cards:
         missingCards = card.get_required_upgrade_cards(card.get("level"), int(targetlevel), card.get("count"), card.get("rarity"))
         missingGold = card.get_required_upgrade_cost(card.get("level"), int(targetlevel), 0, card.get("rarity"))
         imgpath = "res/"+card.name+".png"
-        
+        rarityColor = "#FFFFFF"
+        if card.get("rarity") == "common": # light blue
+            rarityColor = "#00BFFF"
+        elif card.get("rarity") == "rare": # orange
+            rarityColor = "#FFA500"
+        elif card.get("rarity") == "epic": # purple
+            rarityColor = "#800080"
+        elif card.get("rarity") == "legendary": # light pink
+            rarityColor = "#FFB6C1"
+        elif card.get("rarity") == "champion": # yellow
+            rarityColor = "#FFD700"
+
+
         cards_parsed.append(
             {"name":card.get("name"),
              "rarity":card.get("rarity"),
@@ -28,10 +41,11 @@ def process():
              "count":card.get("count"),
              "missingCards":missingCards,
              "missingGold":missingGold,
-             "image_path": imgpath
+             "image_path": imgpath,
+             "rarityColor": rarityColor
              })
 
-    return render_template('cards.html', cards=cards_parsed, targetlevel=targetlevel)
+    return render_template('cards.html', cards=cards_parsed, targetlevel=targetlevel, playerName = player.get("name"))
 
 
 
