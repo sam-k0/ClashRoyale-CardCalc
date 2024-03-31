07be85d90f835e0010544b292302700d6f9c9391
sam-k0 <56673835+sam-k0@users.noreply.github.com>
2024-03-31 18:47:20 +0200

    add search field

--- static/js/cards.js
+++ static/js/cards.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Get the filter select element
     var filterSelect = document.getElementById('filter-select');
-
+    var searchInput = document.getElementById('search-text-filter');
     // Get the card list container
     var cardList = document.getElementById('card-list');
 
     // Function to filter cards based on selected rarity
-    function filterCards(rarity) {
+    function filterCardsRarity(rarity) {
         // Get all card items
         var cardItems = cardList.querySelectorAll('.card');
 
@@ -14,25 +14,40 @@ document.addEventListener('DOMContentLoaded', function() {
         cardItems.forEach(function(cardItem) {
             var cardRarity = cardItem.querySelector('.card-rarity').textContent.toLowerCase();
 
-            console.log(cardRarity);
-            // Show or hide card based on the selected rarity
-            console.log("comparing " + rarity + " with " + cardRarity)
-
             if (rarity === 'all' || cardRarity === rarity) {
-                console.log('show');
                 cardItem.style.display = 'flex'; // Show card
             } else {
-                console.log('hide');
                 cardItem.style.display = 'none'; // Hide card
             }
         });
     }
 
+    // search by text
+    function filterCardsByName(name) {
+        var cardItems = cardList.querySelectorAll('.card');
+        name = name.toLowerCase(); // case insensitive search
+
+        cardItems.forEach(function(cardItem) {
+            var cardName = cardItem.querySelector('.card-name').textContent.toLowerCase();
+            if (cardName.includes(name)) {
+                cardItem.style.display = 'flex'; // Show card
+            } else {
+                cardItem.style.display = 'none'; // Hide card
+            }
+
+        });
+    }
+
     // Add event listener to filter select
     filterSelect.addEventListener('change', function() {
-        filterCards(this.value);
+        filterCardsRarity(this.value);
+    });
+
+    searchInput.addEventListener('keyup', function() {
+        filterCardsByName(this.value);
     });
 
     // Initial filter to display all cards
-    filterCards('all');
+    filterCardsRarity('all');
+
 });
